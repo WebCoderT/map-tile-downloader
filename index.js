44a687a2d778d26093bd6d801b643c2ecf86772d
田正 <tz18609292528@163.com>
2024-12-18 23:23:51 +0800

    正在尝试继承electron

---
+++ index.js
@@ -0,0 +1,155 @@
+const fs = require("fs");
+const path = require("path");
+const request = require("request-promise");
+const headers = require("./src/utils/headers");
+const { URL } = require("./src/config/config");
+
+const mars3d = require('mars3d')
+
+console.log(mars3d)
+
+function calculateTileNumber(latitude, longitude, zoom) {
+  var numTiles = 1 << zoom;
+  var tileX = Math.floor(((longitude + 180) / 360) * numTiles);
+  var tileY = Math.floor(
+    ((1 -
+      Math.log(
+        Math.tan((latitude * Math.PI) / 180) +
+        1 / Math.cos((latitude * Math.PI) / 180)
+      ) /
+      Math.PI) /
+      2) *
+    numTiles
+  );
+  return [tileX, tileY];
+}
+
+const checkoutSingle = async function (x, y, z, filename, maptype, suffix) {
+  var pathname = `tiles/{filename}/{z}/{x}/{y}.${suffix}`.format({
+    x: x,
+    y: y,
+    z: z,
+    filename: filename,
+  });
+  var abspath = path.resolve(pathname);
+  if (!fs.existsSync(abspath)) {
+    await _download(x, y, z, pathname, maptype);
+  } else {
+    fs.stat(abspath, async function (err, stats) {
+      if (err) {
+        await _download(x, y, z, pathname, maptype);
+        return;
+      }
+      if (!stats.size) {
+        fs.unlinkSync(path);
+        await _download(x, y, z, pathname, maptype);
+      }
+    });
+  }
+};
+
+
+
+const _download = async function (x, y, z, filename, maptype) {
+  var url = URL[maptype].format({ x: x, y: y, z: z, s: random(1, 4) });
+  var pathname = path.dirname(filename);
+  mkdirsSync(pathname);
+  if (!fs.existsSync(filename)) {
+    console.log("开始下载：", pathname);
+    await request(
+      {
+        url: url,
+        headers: headers,
+        encoding: "binary",
+      },
+      (err, response) => {
+        if (err) {
+          throw new Error(`出现异常，在${x}${y}${z}`);
+        }
+        fs.writeFileSync(filename, response.body, "binary");
+      }
+    );
+  }
+};
+const random = function (start, end) {
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+};
+
+
+String.prototype.format = function (json) {
+  var temp = this;
+  for (var key in json) {
+    temp = temp.replace("{" + key + "}", json[key]);
+  }
+  return temp;
+};
+
+Number.prototype.toRad = function () {
+  return (this * Math.PI) / 180;
+};
+const mkdirsSync = function (dirpath, mode) {
+  if (!fs.existsSync(dirpath)) {
+    var pathtmp;
+    dirpath.split("/").forEach(function (dirname) {
+      if (pathtmp) {
+        pathtmp = path.join(pathtmp, dirname);
+      } else {
+        pathtmp = dirname;
+      }
+      if (!fs.existsSync(pathtmp)) {
+        if (!fs.mkdirSync(pathtmp, mode)) {
+          return false;
+        }
+      }
+    });
+  }
+  return true;
+};
+
+
+async function procesLatlng(
+  jingdu1,
+  weidu1,
+  jingdu2,
+  weidu2,
+  zoom,
+  filename,
+  maptype,
+  suffix
+) {
+  const tileNumber1 = calculateTileNumber(weidu1, jingdu1, zoom);
+  const tileNumber2 = calculateTileNumber(weidu2, jingdu2, zoom);
+  console.log("西北角：" + tileNumber1, tileNumber2);
+  console.log(`
+        西北角瓦片起始编号：文件夹:${tileNumber1[0]},瓦片id:${tileNumber1[1]}
+        东南角瓦片结束编号：文件夹:${tileNumber2[0]},瓦片id:${tileNumber2[1]}
+        `);
+  const folderNum = tileNumber2[0] - tileNumber1[0] + 1;
+  const fileNum = tileNumber2[1] - tileNumber1[1] + 1;
+  let all = folderNum * fileNum;
+  let total = 0;
+  console.log("\x1b[32m%s\x1b[0m", `瓦片总数共计：${all} 张。`);
+  console.log("\x1b[32m%s\x1b[0m", "...准备拉取...");
+  console.log("\x1b[32m%s\x1b[0m", "...开始拉取...");
+  for (let fileNum = tileNumber1[0]; fileNum <= tileNumber2[0]; fileNum++) {
+    for (let imgNum = tileNumber1[1]; imgNum <= tileNumber2[1]; imgNum++) {
+      console.log(
+        "\x1b[32m%s\x1b[0m",
+        `正在 拉取 供应商【${maptype}】【经纬度范围爬取功能】 第 ${total + 1
+        } 张 瓦片`
+      );
+      await checkoutSingle(fileNum, imgNum, zoom, filename, maptype, suffix);
+      console.log("\x1b[32m%s\x1b[0m", `第 ${total + 1} 张拉取完成。`);
+      total += 1;
+      console.log("\x1b[32m%s\x1b[0m", `剩余 ${all - total} 张。`);
+    }
+  }
+  console.log(
+    "\x1b[31m%s\x1b[0m",
+    `全部拉取完成。。。
+        地图瓦片爬取 全地图 全供应商 【开源软件】
+        认准：Tzzzzzz.
+        真实可靠！ 
+        `
+  );
+}
---
+++ main.js
@@ -0,0 +1,152 @@
+// main.js
+
+// electron 模块可以用来控制应用的生命周期和创建原生浏览窗口
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    // 创建浏览窗口
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    // 加载 index.html
+    mainWindow.loadFile('index.html')
+
+    // 打开开发工具
+    // mainWindow.webContents.openDevTools()
+}
+
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        // 在 macOS 系统内, 如果没有已开启的应用窗口
+        // 点击托盘图标时通常会重新创建一个新窗口
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
+// 对应用程序和它们的菜单栏来说应该时刻保持激活状态, 
+// 直到用户使用 Cmd + Q 明确退出
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+// 在当前文件中你可以引入所有的主进程代码
+// 也可以拆分成几个文件，然后用 require 导入。
+const isMac = process.platform === 'darwin'
+console.log(isMac)
+const template = [
+    // { role: 'appMenu' }
+    ...(isMac
+        ? [{
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideOthers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        }]
+        : []),
+    // { role: 'fileMenu' }
+    {
+        label: 'File',
+        submenu: [
+            isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+    },
+    // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            ...(isMac
+                ? [
+                    { role: 'pasteAndMatchStyle' },
+                    { role: 'delete' },
+                    { role: 'selectAll' },
+                    { type: 'separator' },
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            { role: 'startSpeaking' },
+                            { role: 'stopSpeaking' }
+                        ]
+                    }
+                ]
+                : [
+                    { role: 'delete' },
+                    { type: 'separator' },
+                    { role: 'selectAll' }
+                ])
+        ]
+    },
+    // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forceReload' },
+            { role: 'toggleDevTools' },
+            { type: 'separator' },
+            { role: 'resetZoom' },
+            { role: 'zoomIn' },
+            { role: 'zoomOut' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    // { role: 'windowMenu' }
+    {
+        label: 'Window1',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'zoom', label: '最走走' },
+            ...(isMac
+                ? [
+                    { type: 'separator' },
+                    { role: 'front' },
+                    { type: 'separator' },
+                    { role: 'window' }
+                ]
+                : [
+                    { role: 'close' }
+                ])
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: async () => {
+                    const { shell } = require('electron')
+                    await shell.openExternal('https://electronjs.org')
+                }
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
